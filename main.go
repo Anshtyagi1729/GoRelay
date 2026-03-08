d1436464f28732b851c22244a13db7184d863bbb
Ansh Tyagi <anshtyagi314159@gmail.com>
2026-03-08 11:11:22 +0530

    refactorrrrgit add .

--- main.go
+++ main.go
@@ -9,45 +9,44 @@ import (
 	"time"
 )
 
-var backends = []string{
-	"localhost:8081",
-	"localhost:8082",
-	"localhost:8083",
+type Backend struct {
+	Address string
+	Healthy bool
 }
-var health = map[string]bool{
-	"localhost:8081": true,
-	"localhost:8082": true,
-	"localhost:8083": true,
+
+var backends = []*Backend{
+	{Address: "localhost:8081", Healthy: true},
+	{Address: "localhost:8082", Healthy: true},
+	{Address: "localhost:8083", Healthy: true},
 }
+var backendMu sync.RWMutex
 var current atomic.Int64
-var healthMu sync.RWMutex
 
 func healthchecks() {
 	for {
 		time.Sleep(10 * time.Second)
 		for _, backend := range backends {
-			_, err := http.Get("http://" + backend)
+			_, err := http.Get(backend.Address)
 			if err != nil {
-				healthMu.Lock()
-				health[backend] = false
-				healthMu.Unlock()
+				backendMu.Lock()
+				backend.Healthy = false
+				backendMu.Unlock()
 			} else {
-				healthMu.Lock()
-				health[backend] = true
-				healthMu.Unlock()
+				backendMu.Lock()
+				backend.Healthy = true
+				backendMu.Unlock()
 			}
 		}
 	}
 }
 func proxyhandler(w http.ResponseWriter, r *http.Request) {
 	//copy the incoming request and change the host to backend
-
 	clone := r.Clone(r.Context())
 	idx := current.Add(1) % int64(len(backends))
 	host := backends[idx]
-	healthMu.RLock()
-	ishealthy := health[host]
-	healthMu.RUnlock()
+	backendMu.RLock()
+	ishealthy := host.Healthy
+	backendMu.RUnlock()
 	tried := 0
 	for !ishealthy {
 		if tried == len(backends) {
@@ -56,13 +55,13 @@ func proxyhandler(w http.ResponseWriter, r *http.Request) {
 		}
 		idx = current.Add(1) % int64(len(backends))
 		host = backends[idx]
-		healthMu.RLock()
-		ishealthy = health[host]
-		healthMu.RUnlock()
+		backendMu.RLock()
+		ishealthy = host.Healthy
+		backendMu.RUnlock()
 		tried++
 	}
 	clone.URL.Scheme = "http"
-	clone.URL.Host = backends[idx]
+	clone.URL.Host = backends[idx].Address
 	//roundtrip and handle the error
 	res, err := http.DefaultTransport.RoundTrip(clone)
 	if err != nil {
