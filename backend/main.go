a59b39c54c29bd96f65407c8c358570e25df54de
Ansh Tyagi <anshtyagi314159@gmail.com>
2026-03-08 09:24:52 +0530

    round robbin with 3 backends

--- backend/main.go
+++ backend/main.go
@@ -4,10 +4,22 @@ import (
 	"net/http"
 )
 
+func backend1hand(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<html><body><h1>hello from backend1</h1></body></html>"))
+}
+func backend2hand(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<html><body><h1>hello from backend2</h1></body></html>"))
+}
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("<html><body><h1>hello from backend</h1></body></html>"))
 	})
+	mux1 := http.NewServeMux()
+	mux1.HandleFunc("/", backend1hand)
+	mux2 := http.NewServeMux()
+	mux2.HandleFunc("/", backend2hand)
+	go http.ListenAndServe(":8082", mux1)
+	go http.ListenAndServe(":8083", mux2)
 	http.ListenAndServe(":8081", nil)
 }
 
