5c0bc00d7e640221eb9abc746408897691a2b413
Ansh Tyagi <anshtyagi314159@gmail.com>
2026-03-08 13:19:24 +0530

    added logging

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"io"
+	"log"
 	"net"
 	"net/http"
 	"sync"
@@ -49,6 +49,7 @@ func rateLimiter(w http.ResponseWriter, r *http.Request) bool {
 		} else if clients[client].Count > 100 {
 			clientMu.Unlock()
 			http.Error(w, "Rate limited", http.StatusTooManyRequests)
+			log.Printf("client : %s is rate limited", client)
 			return false
 		} else {
 			clients[client].Count++
@@ -67,6 +68,7 @@ func healthchecks() {
 				backendMu.Lock()
 				backend.Healthy = false
 				backendMu.Unlock()
+				log.Printf("backend %s is down", backend.Address)
 			} else {
 				backendMu.Lock()
 				backend.Healthy = true
@@ -80,6 +82,8 @@ func proxyhandler(w http.ResponseWriter, r *http.Request) {
 	if !rateLimiter(w, r) {
 		return
 	}
+	client, _, _ := net.SplitHostPort(r.RemoteAddr)
+	start := time.Now()
 	clone := r.Clone(r.Context())
 	idx := current.Add(1) % int64(len(backends))
 	host := backends[idx]
@@ -90,6 +94,7 @@ func proxyhandler(w http.ResponseWriter, r *http.Request) {
 	for !ishealthy {
 		if tried == len(backends) {
 			http.Error(w, "no health backend", http.StatusServiceUnavailable)
+			log.Printf("all backends are dead")
 			return
 		}
 		idx = current.Add(1) % int64(len(backends))
@@ -115,11 +120,12 @@ func proxyhandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(res.StatusCode)
 	io.Copy(w, res.Body)
 	res.Body.Close()
+	log.Printf("client:%s backend:%s status:%d duration:%v\n", client, host.Address, res.StatusCode, time.Since(start))
 }
 func main() {
 	mux := http.NewServeMux()
-	fmt.Print("starting proxy server at :3000\n")
 	go healthchecks()
 	mux.HandleFunc("/", proxyhandler)
 	http.ListenAndServe(":3000", mux)
+	log.Printf("reverse proxy started at :3000 for %d backend", len(backends))
 }
